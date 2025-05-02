4c94a8b209c7bef96521c4d0dbe9bfa25382380b
Alarich Herzner <alarich2@hotmail.com>
2025-05-02 10:53:53 +0200

    Add project files.

---
+++ Assignment14Weather/Models/CityWeather.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment14Weather.Models
+{
+    public class CityWeather
+    {
+        public required string CityUniqueCode { get; set; }
+        public required string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
